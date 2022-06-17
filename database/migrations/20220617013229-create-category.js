eed94ad6504f547a492753837ec4e6b9858a0565
Franco Fernandez <75815125+Franckfer@users.noreply.github.com>
2022-06-17 18:42:16 -0300

    [CATEGORIES] - OT226-18 Create Categories model, migration and controller file (#8)
    * added category model and migrations
    
    * added category controller
    
    * exclude packages
    
    * fix in controller and packages
    
    * new fix review
    
    * Update config.js
    
    * fix config.js
    
    * fix review config.example.js

---
+++ database/migrations/20220617013229-create-category.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deleteAt: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Categories');
+  }
+};

4ccd4270c813a8f6e7a62bfea3fd655ceff78893
Alexis Correa <alexis.correa026@gmail.com>
2022-06-17 02:37:20 -0300

    Model and controller with organization name, created

---
+++ database/models/organization.js
@@ -0,0 +1,50 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Organization extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Organization.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    address: {
+    phone: DataTypes.INTEGER,
+    allowNull: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    welcomeText: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    aboutText: DataTypes.TEXT,
+    allowNull: true
+  }, {
+    sequelize,
+    modelName: 'Organization',
+  });
+  return Organization;
+};

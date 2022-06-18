263b88b42c4672f633e82cef064272b6e402aaee
raoole <espinaraul@gmail.com>
2022-06-18 19:59:01 -0400

    error correction

--- database/migrations/20220618150230-create-new.js
+++ database/migrations/20220618150230-create-new.js
@@ -3,7 +3,6 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('News', {
       id: {
-        allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER

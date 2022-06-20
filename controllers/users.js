317d5a53a8c8419b71ab1d4ce667c217fe5e4eb3
Franckfer <francodel4to@gmail.com>
2022-06-19 23:02:21 -0300

    adding users controller

---
+++ controllers/users.js
@@ -0,0 +1,25 @@
+const createHttpError = require('http-errors')
+const { endpointResponse } = require('../helpers/success')
+const { catchAsync } = require('../helpers/catchAsync')
+const { getUserList } = require('../services/users')
+
+module.exports = {
+  getList: catchAsync(async (req, res, next) => {
+    try {
+      const users = await getUserList() // call the service to get the list of users
+      endpointResponse({
+        res,
+        code: 200,
+        status: true,
+        message: 'Users list retrieved successfully',
+        body: users,
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving users list] - [users - GET]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }),
+}

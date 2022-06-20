7fac5720dcae77024c7d220c8ca54d4076436be1
raoole <espinaraul@gmail.com>
2022-06-20 10:12:35 -0400

    delete comments

--- database/models/new.js
+++ database/models/new.js
@@ -4,13 +4,9 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class New extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+   
     static associate(models) {
-      // define association here
+      
       New.belongsTo( models.Category, {
         foreignKey: 'categoryId'
       });

eaa1c49142ef3cff113893673ebd575374baf44b
afrivera <andresrfelip@hotmail.com>
2022-06-20 20:07:03 -0500

    delete association line in role

--- database/models/role.js
+++ database/models/role.js
@@ -10,9 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Role.belongsTo( models.User, {
-        foreignKey: 'roleId'
-      });
+      
     }
   };
   Role.init({

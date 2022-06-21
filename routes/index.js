a41cb0c28e3fb5b0565d80c9ebedbd07cf8eaf12
Franco Fernandez <75815125+Franckfer@users.noreply.github.com>
2022-06-21 12:50:56 -0300

    [ENDPOINT] - OT226-36 GET /users (#24)
    * add route users
    
    * Modification in controllers and Users route
    
    * add services users

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,15 @@ const express = require('express')
 const { get } = require('../controllers/index')
 const organizationRouter = require('./organization')
 const categoryRouter = require('./categories')
+const userRouter = require('./users')
 
 const router = express.Router()
 
 // example of a route with index controller get function
 router.get('/', get)
 
-// users route handler
 router.use('/organization', organizationRouter)
 router.use('/categories', categoryRouter)
+router.use('/users', userRouter)
 
 module.exports = router

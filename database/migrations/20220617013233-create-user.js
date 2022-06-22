671baa5ea911149ad6284d923b6dfa65a42365d5
D'Angello <garcia.dangello@gmail.com>
2022-06-22 18:07:09 -0500

    deletedAt row added

--- database/migrations/20220617013233-create-user.js
+++ database/migrations/20220617013233-create-user.js
@@ -40,7 +40,10 @@ module.exports = {
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
-      }
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {

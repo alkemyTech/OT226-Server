7841729c278d4650e13cdf98afc84d36d120e8d1
Franckfer <francodel4to@gmail.com>
2022-06-22 10:50:50 -0300

    add service login

--- services/users.js
+++ services/users.js
@@ -1,5 +1,6 @@
 const { ErrorObject } = require('../helpers/error')
 const { User } = require('../database/models')
+const bcrypt = require('bcrypt')
 
 module.exports = {
   // query in the database in the users model
@@ -20,6 +21,16 @@ module.exports = {
       if (!user || user.length === 0) {
         throw new ErrorObject('No user found', 404)
       }
+      return user
+    } catch (error) {
+      throw new ErrorObject(error.message, error.statusCode || 500)
+    }
+  },
+  loginUser: async ({ email, password }) => {
+    try {
+      const user = await User.findOne({ where: { email } }) //! where
+      const validatePass = user && bcrypt.compareSync(password, user.password)
+      if (!user || !validatePass) throw new ErrorObject('Invalid credentials', 401)
       return user
     } catch (error) {
       throw new ErrorObject(error.message, error.statusCode || 500)

93298a969d0ed1f30be9e16a69d51fb29f27e12b
Alexis Correa <88632261+AlexAc0226@users.noreply.github.com>
2022-06-22 01:08:56 -0300

    [ENDPOINT] GET /categories/:id (#27)
    * [ENDPOINT] Category Detail
    
    * [ENDPOINT] GET /categories/:id

--- services/categories.js
+++ services/categories.js
@@ -15,3 +15,18 @@ exports.getCategories = async () => {
     throw new ErrorObject(error.message, error.statusCode || 500)
   }
 }
+
+exports.getCategoryById = async (id) => {
+  try {
+    const categoryById = await Category.findOne({
+      where: { id },
+    })
+
+    if (!categoryById) {
+      throw new ErrorObject('Category not found', 404)
+    }
+    return categoryById
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
+}

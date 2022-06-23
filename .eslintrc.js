a78d4f01581efac518fd857d4fef40ef3d2e3857
Raikul <tenchuu@gmail.com>
2022-06-22 21:33:16 -0300

    Fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,7 @@ module.exports = {
     ecmaVersion: 12,
   },
   rules: {
-    'linebreak-style': [
-      'error',
-      process.platform === 'win32' ? 'unix' : 'unix',
-    ],
+    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
     semi: ['error', 'never'],
     'no-param-reassign': ['error', { props: false }],
   },

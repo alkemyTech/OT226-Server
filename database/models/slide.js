00ff0cb9536fc4a38a96aa495e7da38c6b36187c
Franco Fernandez <75815125+Franckfer@users.noreply.github.com>
2022-06-23 19:05:55 -0300

    fix in models slide and organization (#47)

--- database/models/slide.js
+++ database/models/slide.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Slide.belongsTo(Organization, { foreignKey:'organizationId' })
+      Slide.belongsTo(models.Organization, { foreignKey:'organizationId' })
     }
   }
   Slide.init({

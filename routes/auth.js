9cdfabee2999b1a89c650ae7b3830844a7448844
Nicolas Altomonte <tenchuu@gmail.com>
2022-06-23 16:03:23 -0300

    [FIX] Normalized the export method for Schema Validator (#43)
    * Fix exports

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const { post, login } = require('../controllers/users')
-const schemaValidator = require('../middlewares/validator')
+const { schemaValidator } = require('../middlewares/validator')
 const { user } = require('../schemas/user')
 
 router.post('/register', schemaValidator(user), post)

add99c91138c418fdef88b93f570d9e3029f5dfb
Raikul <tenchuu@gmail.com>
2022-06-23 14:08:03 -0300

    Revert Validator - Revert Lock

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,9 +1,11 @@
 const { checkSchema } = require('express-validator')
 const { handleValidator } = require('../helpers/handleValidator')
 
-exports.schemaValidator = (schema) => [
+const schemaValidator = (schema) => [
   checkSchema(schema),
   (req, res, next) => {
     handleValidator(req, res, next)
   },
 ]
+
+module.exports = schemaValidator

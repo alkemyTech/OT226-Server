5fdde99196bead6e504631d1587aeacd273d4318
raul espina <98604463+raoole20@users.noreply.github.com>
2022-06-24 11:18:26 -0400

    [MIDDLEWARE] OT226 - 32  Add JWT logic  (#48)
    * initial settings
    
    * JWT creation middleware
    
    * fix - removed unnecessary function
    
    * fix - add error description
    
    * fix - mproving error message
    
    * update package.json
    
    * fixed logic create jwt
    
    * fixed - move token logic
    
    * normalizing response
    
    Co-authored-by: raoole <espinaraul@gmail.com>

--- services/users.js
+++ services/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const { ErrorObject } = require('../helpers/error')
 const { User } = require('../database/models')
+const { createJWT } = require('../helpers/jwt')
 
 // query in the database in the users model
 exports.getUsers = async () => {
@@ -32,7 +33,8 @@ exports.loginUser = async ({ email, password }) => {
     const user = await User.findOne({ where: { email } })
     const validatePass = user && bcrypt.compareSync(password, user.password)
     if (!user || !validatePass) throw new ErrorObject('Invalid credentials', 401)
-    return user
+    const token = createJWT(user)
+    return { ...user.dataValues, token } // copio valores de user - add token
   } catch (error) {
     throw new ErrorObject(error.message, error.statusCode || 500)
   }

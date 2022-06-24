bfeb2cd92e4fa1c486eec04089ae077fd3ee9566
afrivera <andresrfelip@hotmail.com>
2022-06-24 08:59:10 -0500

    config controller to send welcome email

--- controllers/email.js
+++ controllers/email.js
@@ -2,21 +2,28 @@ const { sendEmailTo } = require('../services/email')
 const { getOrganizations } = require('../services/organization')
 
 module.exports = {
-  welcomeEmail: async (user) => {
+  /**
+   *
+   * @param {string} emailUser
+   */
+  welcomeEmail: async (emailUser) => {
     try {
-      const [organization] = await getOrganizations()
+      // should implement a service of organization ById to get fields
+      const [organization2] = await getOrganizations()
       const {
-        image, name, phone, email,
-      } = organization
+        image, name, phone, email = 'here email ong', welcomeText = 'here welcome text', address,
+      } = organization2
+      // this data is to send variables to welcome template
       const data = {
         img: image,
-        name,
-        contact: `${phone} - ${email}`,
+        nameOrganization: name,
+        welcomeText,
+        contact: `address: ${address} - phone: ${phone} - email: ${email}`,
         subject: `welcome to ${name}`,
       }
 
       // here we use the email service
-      await sendEmailTo(user.email, data)
+      await sendEmailTo(emailUser, data)
     } catch (error) {
       console.error(error) // eslint-disable-line
     }
--- services/email.js
+++ services/email.js
@@ -2,17 +2,21 @@ const sgMail = require('@sendgrid/mail')
 const read = require('fs')
 const ejs = require('ejs')
 const { join } = require('path')
-
-exports.sendEmailTo = async (email, data) => {
+/**
+ *
+ * @param {string} emailUser
+ * @param {object} data - data contains the variables to send to welcome template
+ */
+exports.sendEmailTo = async (emailUser, data) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   // we use welcome template here
   const str = read.readFileSync(join(__dirname, '../views/welcomeTemplate.ejs'), 'utf-8')
-  const body = ejs.render(str)(data)
+  const body = ejs.render(str, data)
   const msg = {
-    to: email, // Change to your recipient
-    from: data.email, // Change to your verified sender
+    to: emailUser, // Change to your recipient
+    from: 'nicolas.altomonte@gmail.com', // Change to your verified sender
     subject: data.subject,
-    text: 'OT226-28',
+    text: data.welcomeText,
     html: body,
   }
   try {
@@ -20,6 +24,6 @@ exports.sendEmailTo = async (email, data) => {
     await sgMail.send(msg)
     console.log('Email sent') // eslint-disable-line
   } catch (error) {
-    console.error(error) // eslint-disable-line    
+    console.error('here',error) // eslint-disable-line    
   }
 }

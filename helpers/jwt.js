b1196e10e2bceed27012a96649174c7ebc3eee0d
raul espina <98604463+raoole20@users.noreply.github.com>
2022-06-27 08:02:24 -0400

    [ENDPOINT] OT226-37 authenticated user data (#53)
    * authenticated user data
    
    * fixed decryptJWT
    
    * fixed rename controllers
    
    Co-authored-by: raoole <espinaraul@gmail.com>

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -14,3 +14,18 @@ exports.createJWT = (body) => {
 }
 
 exports.validateJWT = async (token) => jwt.verify(token, process.env.JWT_KEY)
+
+exports.decryptJWT = (header) => {
+  const token = header.authorization
+  try {
+    const { id, firstName, roleId } = jwt.verify(token, process.env.JWT_KEY)
+    return {
+      id,
+      firstName,
+      roleId,
+    }
+  } catch (error) {
+    const message = `[Token not provided or invalid] - [Access - Denied]: ${error.message}`
+    return message
+  }
+}

886c82729ec9631ac3090c916b9e52fb6f93142c
raul espina <98604463+raoole20@users.noreply.github.com>
2022-06-27 11:54:47 -0400

    fix error exports auth (#56)
    Co-authored-by: raoole <espinaraul@gmail.com>

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
 
-const { post, login, getInformation } = require('../controllers/users')
+const { post, login, getUserByToken } = require('../controllers/users')
 const { schemaValidator } = require('../middlewares/validator')
 const { user } = require('../schemas/user')
 const { isAuthenticatedUser } = require('../middlewares/auth')
 
 router.post('/register', schemaValidator(user), post)
 router.post('/login', login)
-router.get('/me', isAuthenticatedUser, getInformation)
+router.get('/me', isAuthenticatedUser, getUserByToken)
 
 module.exports = router

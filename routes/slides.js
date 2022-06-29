913ed8c1b0a1026da7280cd830e69809325d5c0e
nico <nicolas.escudero00@gmail.com>
2022-06-29 19:25:14 -0300

    adding endpoint get slide by id

--- routes/slides.js
+++ routes/slides.js
@@ -1,11 +1,12 @@
 const express = require('express')
 // const { schemaValidator } = require('../middlewares/validator')
-const { get } = require('../controllers/slides')
+const { get, getSlideById } = require('../controllers/slides')
 const { verifyUsers } = require('../middlewares/auth')
 const { isAdmin } = require('../middlewares/isAdmin')
 
 const router = express.Router()
 
 router.get('/', verifyUsers, isAdmin, get)
+router.get('/:id', verifyUsers, isAdmin, getSlideById)
 
 module.exports = router

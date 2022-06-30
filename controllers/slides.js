1009a4fce68fb7b84ce47de3c9dc9c65a6641266
Andres Felipe Rivera <82002776+afrivera@users.noreply.github.com>
2022-06-30 09:05:27 -0500

    config endpoing get /slides (#67)

---
+++ controllers/slides.js
@@ -0,0 +1,24 @@
+const createHttpError = require('http-errors')
+const { getSlides } = require('../services/slides')
+const { endpointResponse } = require('../helpers/success')
+const { catchAsync } = require('../helpers/catchAsync')
+
+// example of a controller. First call the service, then build the controller method
+module.exports = {
+  get: catchAsync(async (req, res, next) => {
+    try {
+      const response = await getSlides()
+      endpointResponse({
+        res,
+        message: 'slides retrieved successfully',
+        body: response,
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving index] - [index - GET]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }),
+}

62a07f0902fd57da1266af0bd17e8ab419824f05
Andres Felipe Rivera <82002776+afrivera@users.noreply.github.com>
2022-06-30 20:08:57 -0500

    config endpoint Delete /slides/:id (#73)

--- controllers/slides.js
+++ controllers/slides.js
@@ -1,5 +1,5 @@
 const createHttpError = require('http-errors')
-const { getSlides, getSlideById } = require('../services/slides')
+const { getSlides, getSlideById, deleteSlide } = require('../services/slides')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
 
@@ -33,6 +33,22 @@ module.exports = {
       const httpError = createHttpError(
         error.statusCode,
         `[Error retrieving slide] - [Slide - GET]: ${error.message}`,
+      )
+      next(httpError)
+    }
+  }),
+  destroy: catchAsync(async (req, res, next) => {
+    try {
+      const response = await deleteSlide(req.params.id)
+      endpointResponse({
+        res,
+        message: 'slides deleted succesfully',
+        body: response,
+      })
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error deleting Slide] - [index - DELETE]: ${error.message}`,
       )
       next(httpError)
     }

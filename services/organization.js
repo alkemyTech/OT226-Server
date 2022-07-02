d670d173a8850c39eb6d5d2c91e6dd2bd63dd56b
raoole <espinaraul@gmail.com>
2022-07-01 20:23:03 -0400

    returning social networks in organization

--- services/organization.js
+++ services/organization.js
@@ -4,18 +4,13 @@ const { Organization } = require('../database/models')
 // example of a service
 exports.getOrganizations = async () => {
   try {
-    const redes = {
-      facebook: 'https://facebook/test_1',
-      instagram: 'https://instagram/test_1',
-      tweter: 'https://tweter/test_1',
-    }
     const getOrganizations = await Organization.findAll({
-      attributes: ['name', 'image', 'phone', 'address'],
+      attributes: ['name', 'image', 'phone', 'address', 'facebookUrl', 'instagramUrl', 'linkedinUrl'],
     })
     if (!getOrganizations) {
       throw new ErrorObject('No index found', 404)
     }
-    return { getOrganizations, redes }
+    return { getOrganizations }
   } catch (error) {
     throw new ErrorObject(error.message, error.statusCode || 500)
   }

b46d4f58b8e9a1eb3680d325a5838c96e21de5fa
afrivera <andresrfelip@hotmail.com>
2022-07-04 15:55:38 -0500

    config endpoin update testimonials /testimonials/:id

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const authRouter = require('./auth')
 const activityRouter = require('./activities')
 const contactRouter = require('./contacts')
 const slideRouter = require('./slides')
+const testimonialRouter = require('./testimonials')
 
 const router = express.Router()
 
@@ -21,5 +22,6 @@ router.use('/auth', authRouter)
 router.use('/activities', activityRouter)
 router.use('/contacts', contactRouter)
 router.use('/slides', slideRouter)
+router.use('/testimonials', testimonialRouter)
 
 module.exports = router
---
+++ services/testimonials.js
@@ -0,0 +1,21 @@
+const { ErrorObject } = require('../helpers/error')
+const { Testimonial } = require('../database/models')
+
+// example of a service
+
+exports.updateTestimonial = async (body, id) => {
+  try {
+    const testimonial = await Testimonial.findOne({
+      where: { id },
+    })
+    if (!testimonial) {
+      throw new ErrorObject('Testimonial not found', 404)
+    }
+    await Testimonial.update(body, {
+      where: { id },
+    })
+    return testimonial
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
+}

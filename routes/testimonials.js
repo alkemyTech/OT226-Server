ff5b7120ae1e8a11c945d2ca3b525b21b02332a8
D'Angello <garcia.dangello@gmail.com>
2022-07-05 19:11:04 -0500

    create testimonial added

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,12 +1,13 @@
 const express = require('express')
-// const { schemaValidator } = require('../middlewares/validator')
-// const { putTestimonial } = require('../schemas/testimonial')
-const { put } = require('../controllers/testimonials')
+const { schemaValidator } = require('../middlewares/validator')
+const { postTestimonial } = require('../schemas/testimonial')
+const { put, post } = require('../controllers/testimonials')
 const { verifyUsers } = require('../middlewares/auth')
 const { isAdmin } = require('../middlewares/isAdmin')
 
 const router = express.Router()
 
 router.put('/:id', verifyUsers, isAdmin, /* schemaValidator(putTestimonial), */ put)
+router.post('/', verifyUsers, isAdmin, schemaValidator(postTestimonial), post)
 
 module.exports = router

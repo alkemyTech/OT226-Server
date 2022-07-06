c122c9bb735d049f182da2dc999fe18afa32ca09
afrivera <andresrfelip@hotmail.com>
2022-07-06 12:50:25 -0500

    fix schema comment

--- schemas/comment.js
+++ schemas/comment.js
@@ -12,24 +12,24 @@ exports.comment = {
   },
   user_id: {
     exists: {
-      errorMessage: 'There must be a body',
+      errorMessage: 'There must be a user_id',
     },
     notEmpty: {
-      errorMessage: 'body is empty',
+      errorMessage: 'user_id is empty',
     },
     isInt: {
-      errorMessage: 'The body must be a integer',
+      errorMessage: 'The user_id must be a integer',
     },
   },
   news_id: {
     exists: {
-      errorMessage: 'There must be a body',
+      errorMessage: 'There must be a news_id',
     },
     notEmpty: {
-      errorMessage: 'body is empty',
+      errorMessage: 'news_id is empty',
     },
     isInt: {
-      errorMessage: 'The body must be a integer',
+      errorMessage: 'The news_id must be a integer',
     },
   },
 }

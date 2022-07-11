e126d068c471143dd888ea0dc57ddb3b9d796e7d
Franco Fernandez <75815125+Franckfer@users.noreply.github.com>
2022-07-11 14:52:54 -0300

    added news pagination (#100)

--- routes/news.js
+++ routes/news.js
@@ -3,13 +3,14 @@ const express = require('express')
 const { schemaValidator } = require('../middlewares/validator')
 const { news } = require('../schemas/new')
 const {
-  post, getById, destroy, put,
+  post, get, getById, destroy, put,
 } = require('../controllers/news')
 const { isAdmin } = require('../middlewares/isAdmin')
 const { verifyUsers } = require('../middlewares/auth')
 
 const router = express.Router()
 
+router.get('/', get)
 router.post('/', verifyUsers, isAdmin, schemaValidator(news), post)
 router.get('/:id', getById)
 router.delete('/:id', verifyUsers, isAdmin, destroy)

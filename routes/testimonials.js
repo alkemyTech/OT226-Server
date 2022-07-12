8f064ee927faefbe9fe11ff95fc0b822dc872f06
raoole <espinaraul@gmail.com>
2022-07-12 12:36:08 -0400

    creating paginator in testimonials

--- routes/testimonials.js
+++ routes/testimonials.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const { schemaValidator } = require('../middlewares/validator')
 const { postTestimonial } = require('../schemas/testimonial')
-const { put, post, destroy } = require('../controllers/testimonials')
+const {
+  get, put, post, destroy,
+} = require('../controllers/testimonials')
 const { verifyUsers } = require('../middlewares/auth')
 const { isAdmin } = require('../middlewares/isAdmin')
 
 const router = express.Router()
 
+router.get('/', get)
 router.put('/:id', verifyUsers, isAdmin, /* schemaValidator(putTestimonial), */ put)
 router.post('/', verifyUsers, isAdmin, schemaValidator(postTestimonial), post)
 router.delete('/:id', verifyUsers, isAdmin, destroy)
--- services/testimonials.js
+++ services/testimonials.js
@@ -1,7 +1,33 @@
 const { ErrorObject } = require('../helpers/error')
 const { Testimonial } = require('../database/models')
 
-// example of a service
+exports.getTestimonials = async (req) => {
+  const { page = 1 } = req.query
+
+  const getUrl = `${req.protocol}://${req.get('host')}${req.baseUrl}`
+
+  const options = {
+    limit: 10,
+    offset: (+page > 0 ? (+page - 1) : 0) * 10,
+  }
+
+  try {
+    const { count, rows: testimonials } = await Testimonial.findAndCountAll(options)
+    const totalPages = Math.ceil(count / options.limit)
+
+    if (totalPages < page) throw new ErrorObject('Testimonials not found', 404)
+
+    return {
+      prev: (page > 1) ? `${getUrl}?page=${+page - 1}` : null,
+      next: (page < totalPages) ? `${getUrl}?page=${+page + 1}` : null,
+      currentPage: page,
+      totalPages,
+      testimonials,
+    }
+  } catch (error) {
+    throw new ErrorObject(error.message, error.statusCode || 500)
+  }
+}
 
 exports.updateTestimonial = async (body, id) => {
   try {
